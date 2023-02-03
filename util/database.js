a455b6933133ac0da4649041ae7a9a43db319e2e
Shwetha-703 <shweta.shingare11@gmail.com>
2023-02-03 14:52:58 +0530

    Adding the initial commit

---
+++ util/database.js
@@ -0,0 +1,30 @@
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
+
+let _db;
+
+const mongoConnect = callback =>{
+  MongoClient.connect('mongodb+srv://mongotutorial:plmnbvcxzaq@myatlasclusteredu.eynyopk.mongodb.net/shop?retryWrites=true&w=majority')
+  .then(client =>{
+    console.log('Connected!');
+    _db = client.db();
+    callback();
+  })
+  .catch(err => {
+    console.log(err);
+    throw err;
+  });
+};
+
+const getDb = () => {
+  if(_db){
+    return _db;
+  }
+  throw 'No DB found!';
+}
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
+
+//module.exports = mongoConnect;
+//module.exports = getDb;
